3557a33c665cd1797dea297147e4d9786e4d594a
SirSabek <mmsabek99@gmail.com>
2023-06-17 11:21:07 +0200

    update Hotel Controller

--- HotelListingAPI/Controllers/HotelController.cs
+++ HotelListingAPI/Controllers/HotelController.cs
@@ -1,11 +1,55 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using HotelListingAPI.DTOs;
+using HotelListingAPI.IRepository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HotelListingAPI.Controllers
+namespace HotelListingAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HotelController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class HotelController : ControllerBase
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<HotelController> _logger;
+    private readonly IMapper _mapper;
+
+    public HotelController(IMapper mapper, ILogger<HotelController> logger, IUnitOfWork unitOfWork)
     {
+        _mapper = mapper;
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetHotels()
+    {
+        try
+        {
+            var hotels = await _unitOfWork.Hotels.GetAll();
+            return Ok(_mapper.Map<List<HotelDTO>>(hotels));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Something went wrong in the {nameof(GetHotels)}");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please Try Again Later.");
+        }
+    }
+
+    [HttpGet("{id:int}", Name = "GetHotel")]
+    public async Task<IActionResult> GetHotel(int id)
+    {
+        try
+        {
+            var hotel = await _unitOfWork.Hotels.Get(q => q.Id == id, new List<string> { "Country" });
+            return Ok(_mapper.Map<HotelDTO>(hotel));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Something went wrong in the {nameof(GetHotel)}");
+            return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error. Please Try Again Later.");
+        }
     }
 }
