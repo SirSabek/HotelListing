350c221d40e39daab6e93d57ada65f29ef508142
SirSabek <mmsabek99@gmail.com>
2023-06-18 12:23:23 +0200

    Seperating some Configurations From Program Class

--- HotelListingAPI/ServiceExtensions.cs
+++ HotelListingAPI/ServiceExtensions.cs
@@ -1,5 +1,8 @@
-﻿using HotelListingAPI.Data;
+﻿using System.Text;
+using HotelListingAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HotelListingAPI;
 
@@ -9,6 +12,30 @@ public static class ServiceExtensions
     {
         var builder = services.AddIdentityCore<APIUser>(q => q.User.RequireUniqueEmail = true);
         builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
-        builder.AddSignInManager<SignInManager<APIUser>>().AddEntityFrameworkStores<Context>().AddDefaultTokenProviders();
+        builder.AddEntityFrameworkStores<Context>().AddDefaultTokenProviders();
+    }
+
+    public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSettings = configuration.GetSection("Jwt");
+        var key = "2a5979f0 - 0d15 - 11ee - be56 - 0242ac120002"; //Environment.GetEnvironmentVariable("HOTEL-KEY");
+
+        services.AddAuthentication(o =>
+        {
+            o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(o =>
+        {
+            o.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.GetSection("Issuer").Value,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+            };
+        });
+
     }
 }
