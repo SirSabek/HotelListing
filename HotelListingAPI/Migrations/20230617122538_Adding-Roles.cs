31366e8d41c79f421c7b479fd938b3ef5d37c0ce
SirSabek <mmsabek99@gmail.com>
2023-06-18 12:24:05 +0200

    adding latest migrations

---
+++ HotelListingAPI/Migrations/20230617122538_Adding-Roles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4f29875e-4dd8-445f-b474-4b09e435b1ef", null, "Administrator", "ADMINISTRATOR" },
+                    { "86ab6d1f-d0fc-45c8-b1a1-02dbfea35f12", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4f29875e-4dd8-445f-b474-4b09e435b1ef");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "86ab6d1f-d0fc-45c8-b1a1-02dbfea35f12");
+        }
+    }
+}
